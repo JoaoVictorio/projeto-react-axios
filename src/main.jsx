2cd1410834f767d21a9bc757a184d27752149794
Joao <joaoprodutor92@gmail.com>
2023-09-05 21:42:13 -0300

    importa biblioteca react-router-dom e cria obj de Config

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+
+import { createBrowserRoute, RouterProvider, Route } from 'react-router-dom';
+
 import './index.css'
+
+// Obj de configuração
+const router = createBrowserRoute([
+// Elemento princ. que vai abrigar as páginas
+{
+  element: <App />,
+  children: [
+    {
+      path: "/"
+    },
+    {
+      path: "/new"
+    },
+  ],
+},
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
